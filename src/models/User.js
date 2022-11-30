1dcd62757ca1cbbdac32517ca6d11170b2234ff8
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-11-30 18:31:10 -0300

    feat: Requisito 2 - Criando Model de User :sparkles:

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+  return user;
+};
