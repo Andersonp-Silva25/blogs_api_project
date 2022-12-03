a81ee4c47bd07d3bd381eec8d5d95b44e70dce51
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-12-03 16:25:51 -0300

    feat: Requisito 11 - Criando Model de PostCategory com associação N:N entre BlogPost e Category :sparkles:

---
+++ src/models/PostCategory.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const postCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+
+  postCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: postCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      as: 'Category',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      through: postCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      as: 'BlogPost',
+    });
+  }
+
+  return postCategory;
+};
