1f4e996605bf70ee8860837ca77e3dd602a18663
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-12-03 14:08:41 -0300

    feat: Requisito 7 - Criando Model de Category :sparkles:

---
+++ src/models/Category.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true,
+  });
+  return category;
+};
